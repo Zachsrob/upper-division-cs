785a3246707020e1d6761acb2c5364be33b27df0
Zachsrob <146136668+Zachsrob@users.noreply.github.com>
2023-09-26 14:17:00 -0700

    Update git-config.sh

--- dsa-23au/scripts/git-config.sh
+++ dsa-23au/scripts/git-config.sh
@@ -1,1 +1,4 @@
-146136668+Zachsrob@users.noreply.github.com
+#!/bin/sh
+
+# Enable us to push, through email privacy fatures
+git config --global user.email 146136668+Zachsrob@users.noreply.github.com
